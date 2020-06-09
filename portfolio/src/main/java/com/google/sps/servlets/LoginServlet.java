da40a211f648ad90152cf1795bbceae07f061d5a
Eashan Bhatt <eash130@gmail.com>
2020-06-09 16:45:50 +0000

    Prevent user from posting without authentication

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -14,13 +14,9 @@
 
 package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.JsonObject;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,68 +26,22 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
-  private static final String USER_INFO = "User";
-  private static final String NICKNAME = "nickname";
-  private static final String USER_ID = "userId";
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html");
     UserService userService = UserServiceFactory.getUserService();
-    if (!userService.isUserLoggedIn()) {
-      // Prompt user to login to their Google account.
-      String loginLink = userService.createLoginURL("/login");
-      response.getWriter().println("<h3>Welcome!</h3>");
-      response.getWriter().println("<p>Click <a href=\"" + loginLink + "\">here</a> to login.</p>");
-      response.getWriter().println("<p>Click <a href=\"/\">here</a> to return to the home page</p>");
-    } else if (getUserNickname(userService.getCurrentUser().getUserId()) == "") {
-      // Prompt user to select a nickname to go by.
-      String userEmail = userService.getCurrentUser().getEmail();
-      response.getWriter().println("<h3>Welcome " + userEmail + "!</h3>");
-      response.getWriter().println("<p>Choose a nickname for everyone to call you by.</p>");
-      response.getWriter().println("<form action=\"/login\" method=\"POST\">");
-      response.getWriter().println("<input type=\"text\" name=\"nickname\" />");
-      response.getWriter().println("<input type=\"submit\" value=\"Confirm\" />");
-      response.getWriter().println("</form>");
-    } else {
-      // user is good to go
-      String nickname = getUserNickname(userService.getCurrentUser().getUserId());
-      String logoutLink = userService.createLogoutURL("/login");
-      response.getWriter().println("<h3>Welcome " + nickname + "!</h3>");
-      response.getWriter().println("<p>Your account is registered and you can post comments!</p>");
-      response.getWriter().println("<p>Click <a href=\"" + logoutLink + "\">here</a> to logout.</p>");
-      response.getWriter().println("<p>Click <a href=\"/\">here</a> to return to the home page</p>");
-    }
-  }
 
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String nickname = request.getParameter(NICKNAME);
-    if (!nickname.isEmpty()) {
-      Entity userEntity = new Entity(USER_INFO);
-      userEntity.setProperty(NICKNAME, nickname);
-      UserService userService = UserServiceFactory.getUserService();
-      userEntity.setProperty(USER_ID, userService.getCurrentUser().getUserId());
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-      datastore.put(userEntity);
+    // Assume user is not logged in and respond with a login link that redirects to "/" after login
+    // link is either a login URL or a logout URL depending on the current status of the user
+    String userEmail = "";
+    String link = userService.createLoginURL("/");
+    if (userService.isUserLoggedIn()) {
+      userEmail = userService.getCurrentUser().getEmail();
+      link = userService.createLogoutURL("/");
     }
-    response.sendRedirect("/login");
-  }
-
-  /**
-   * Returns the nickname of the user with id, or empty String if the user has not set a nickname.
-   */
-  private String getUserNickname(String userId) {
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Query query =
-        new Query(USER_INFO)
-            .setFilter(new Query.FilterPredicate(USER_ID, Query.FilterOperator.EQUAL, userId));
-    PreparedQuery results = datastore.prepare(query);
-    Entity entity = results.asSingleEntity();
-    if (entity == null) {
-      return "";
-    }
-    String nickname = (String) entity.getProperty(NICKNAME);
-    return nickname;
+    JsonObject loginInfo = new JsonObject();
+    loginInfo.addProperty("email", userEmail);
+    loginInfo.addProperty("authenticationURL", link);
+    response.setContentType("application/json");
+    response.getWriter().println(loginInfo);
   }
 }
